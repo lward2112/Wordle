34401157393ec502bbac16fd478535c5093f03df
Lauren Ward <wardlauren@google.com>
2023-09-27 13:35:31 -0400

    Split main Wordle function into several functions.

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.wordle.ui.theme.WordleTheme
@@ -37,12 +40,11 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-    enum class GuessResult {
-        CORRECT, WRONG_POSITION, NOT_IN_WORD
-    }
 }
 
-
+enum class GuessResult {
+    CORRECT, WRONG_POSITION, NOT_IN_WORD
+}
 
 @Composable
 fun Wordle(targetWord: String) {
@@ -59,58 +61,85 @@ fun Wordle(targetWord: String) {
             onValueChange = { newText ->
                 guess = newText
             },
-            label = {Text("Enter your guess here.")}
+            label = { Text("Enter your guess here.") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = { // add log statement here
+             }
+            )
         )
 
-        val results = mutableListOf<MainActivity.GuessResult>()
+         checkGuess(guess = guess, targetWord = targetWord)
 
-        // Loop through indexes of guess
-        // Check if the letters in both words (guess and targetWord) match
-        // Check if guess letter is anywhere in targetWord
+    }
+}
 
-        if (guess.length == 5) {
-            Log.d("logdlauren", "Guess length is 5")
-            for (guessIndex in 0 until guess.length) {
-                var result = MainActivity.GuessResult.NOT_IN_WORD
-                Log.d("logalauren", "${guess[guessIndex]}")
-                if (guess[guessIndex] == targetWord[guessIndex]) {
-                    result = MainActivity.GuessResult.CORRECT
-                    Log.d("logblauren", "${guess[guessIndex]} is in the correct position")
-                } else if (guess[guessIndex] !== targetWord[guessIndex]) {
-                    for (targetIndex in 0 until targetWord.length) {
-                        if (guess[guessIndex] == targetWord[targetIndex]) {
-                            result = MainActivity.GuessResult.WRONG_POSITION
-                            Log.d("logclauren", "${guess[guessIndex]} is in the word, but in the wrong position")
-                            break
-                        }
-                    }
-                }
-                results.add(result)
-            }
-            Log.d("logelauren", "$results")
-            Row () {
-                for (guessIndex in 0 until guess.length) {
-                    var letter = guess[guessIndex]
-                    var result = results[guessIndex]
-                    var color = Color.DarkGray
+// Loop through indexes of guess
+// Check if the letters in both words (guess and targetWord) match
+// Check if guess letter is anywhere in targetWord
+@Composable
+fun checkGuess(guess: String, targetWord: String) {
+     if (guess.length != 5) return
 
-                    if (result == MainActivity.GuessResult.CORRECT) {
-                        color = Color.Green
-                    } else if (result == MainActivity.GuessResult.WRONG_POSITION) {
-                        color = Color.Yellow
-                    } else {
-                        color = Color.DarkGray
-                    }
-                    Text("$letter",
-                        color = color,
-                        fontSize = 50.sp)
-                }
-            }
+    val results = getResults(guess, targetWord)
+
+    Row() {
+        for (guessIndex in 0 until guess.length) {
+            var letter = guess[guessIndex]
+            var result = results[guessIndex]
+            Text(
+                text = "$letter",
+                color = getColor(result),
+                fontSize = 50.sp)
         }
     }
 }
 
 
+fun getResults(guess: String, targetWord: String): List<GuessResult> {
+    val results = mutableListOf<GuessResult>()
+
+    for (guessIndex in 0 until guess.length) {
+        var result = GuessResult.NOT_IN_WORD
+        Log.d("logalauren", "${guess[guessIndex]}")
+        if (guess[guessIndex] == targetWord[guessIndex]) {
+            result = GuessResult.CORRECT
+            Log.d("logblauren", "${guess[guessIndex]} is in the correct position")
+        } else if (guess[guessIndex] !== targetWord[guessIndex]) {
+            for (targetIndex in 0 until targetWord.length) {
+                if (guess[guessIndex] == targetWord[targetIndex]) {
+                    result = GuessResult.WRONG_POSITION
+                    Log.d(
+                        "logclauren",
+                        "${guess[guessIndex]} is in the word, but in the wrong position"
+                    )
+                    break
+                }
+            }
+        }
+        results.add(result)
+        Log.d("logelauren", "$results")
+    }
+    var myList = results.toList()
+    Log.d("Logresults", "$myList")
+    return results.toList()
+}
+
+fun getColor(result: GuessResult): Color {
+    when (result) {
+        GuessResult.CORRECT -> {
+            return Color.Green
+        }
+
+        GuessResult.WRONG_POSITION -> {
+            return Color.Yellow
+        }
+
+        else -> {
+            return Color.DarkGray
+        }
+    }
+}
 
 @Preview
 @Composable
